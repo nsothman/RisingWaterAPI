40ffbbb3e5ca3fe05f797977eeb1ff50af32416a
Abdulla Ahmadkhan <45553812+AbdullahAhmadKhan@users.noreply.github.com>
2019-10-20 00:59:31 +0300

    Update and rename main.py to api.py
    vcvc

---
+++ api.py
@@ -0,0 +1,20 @@
+from flask import request, Flask
+from flask_restful import Resource, Api
+import pandas as pd
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class main(Resource):
+    def post(self):
+        data = pd.read_csv('data.csv')
+        date = request.get_json()
+        i = 0
+        while(str(date['date']) >= str(data['Time'][i])):
+            GMSL = data['GMSL'][i]
+            i += 1
+        return {'GMSL': GMSL}
+
+api.add_resource(main, '/')
+app.run(debug = True)
