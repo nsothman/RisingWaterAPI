5fc97df73dfbd611cc2cbe03b9c1cfed5258b402
Abdulla Ahmadkhan <45553812+AbdullahAhmadKhan@users.noreply.github.com>
2019-10-20 02:46:02 +0300

    fgfg

--- api.py
+++ api.py
@@ -5,14 +5,13 @@ import pandas as pd
 app = Flask(__name__)
 api = Api(app)
 
-
 class main(Resource):
     def post(self):
-        data = pd.read_csv('data.csv')
+        data = pd.read_csv('Final_data.csv')
         date = request.get_json()
         i = 0
-        while(str(date['date']) >= str(data['Time'][i])):
-            GMSL = data['GMSL'][i]
+        while(float((date['date'])) >= float(data['Time'][i])):
+            GMSL = float(data['Mean Value'][i])
             i += 1
         return {'GMSL': GMSL}
 
